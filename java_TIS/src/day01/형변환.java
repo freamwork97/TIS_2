977b1d5e42a46c3c34662dade3f3273d524dd491
freamwork97 <jsy11123@naver.com>
2023-06-17 19:55:27 +0900

    java_TIS

---
+++ java_TIS/src/day01/형변환.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class 형변환 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10; //정수 타입 a 
+		double b = a; //실수 타입 b 자동형변환 
+		System.out.println(b); 
+		
+		double c = 3.14;
+		int d = (int)c; //강제형변환
+		System.out.println(d);
+	}
+
+}
